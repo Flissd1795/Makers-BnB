d4dcc036a166cfbb52c052d7ea74f0641992f58a
Emily Sadler <emily@sadler.gg>
2025-01-22 10:26:54 +0000

    tests and code completed for checking if input is valid in homes

--- lib/homes_repository.py
+++ lib/homes_repository.py
@@ -22,10 +22,9 @@ class HomesRepository:
     def find(self, id):
         home = self.connection.execute("SELECT * FROM homes WHERE id = {};".format(id))
         return Home(home[0]["id"], home[0]["title"], home[0]["description"], home[0]["location"], home[0]["price_per_night"], home[0]["user_id"])
-   main
 
 
-dating
+
     def view_booked_dates(self, home_id):
         requests = self.connection.execute("SELECT * FROM requests WHERE home_id = %s;", [home_id])
         booked_dates = []
