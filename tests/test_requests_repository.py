f71082b36450a395aa5d3614575a7db8994429e9
Fliss <fliss_d@hotmail.co.uk>
2025-01-22 12:40:35 +0000

    requests repo and tests

--- tests/test_requests_repository.py
+++ tests/test_requests_repository.py
@@ -1,69 +1,45 @@
-from lib.requests import Request
-from lib.requests_repository import RequestRepository
+# from lib.requests import Request
+# from lib.requests_repository import RequestRepository
 
-def test_get_all_requests(db_connection):
-    db_connection.seed("seeds/makers_bnb.sql")
-    repository = RequestRepository(db_connection)
-    requests = repository.get_all_requests()
-    assert requests == ["Request(1, Unseen, 2000-01-01, 1, 1, 2000-02-01, 2000-03-01), Request(2, Confirmed, 2000-01-01, 1, 1, 2000-02-01, 2000-03-01)"]
-    # both examples added to database
-
-def test_get_requests_by_user():
-
-
-
-def get_requests_to_user_homes():
-
-
-
-
-
-
-
-
-# def test_get_all_users(db_connection):
+# def test_get_all_requests(db_connection):
 #     db_connection.seed("seeds/makers_bnb.sql")
-#     repository = UserRepository(db_connection)
-#     users = repository.all_users()
-#     assert users == ["User(1, test_username, test@email.com, test_password)", "User(2, test_username2, test2@email.com, test_password2)"]
+#     repository = RequestRepository(db_connection)
+#     requests = repository.get_all_requests()
+#     assert requests == ["Request(1, unseen, 2000-01-01, 1, 1, 2000-02-05, 2000-02-07)", "Request(2, confirmed, 2000-01-01, 1, 1, 2000-02-05, 2000-02-07)"]
+#     # updated from database
 
-# def test_create_user(db_connection):
+# def test_create_request(db_connection):
 #     db_connection.seed("seeds/makers_bnb.sql")
-#     repository = UserRepository(db_connection)
-#     repository.create_user("test_username3", "test3@email.com", "test_password3")
-#     users = repository.all_users()
-#     assert users == ["User(1, test_username, test@email.com, test_password)", "User(2, test_username2, test2@email.com, test_password2)", "User(3, test_username3, test3@email.com, test_password3)"]
+#     repository = RequestRepository(db_connection)
+#     repository.create_request("Unseen3", 2000-01-01, 3, 3, 2000-02-01, 2000-03-01)
+#     requests = repository.get_all_requests()
+#     assert requests == ["Request(1, unseen, 2000-01-01, 1, 1, 2000-02-05, 2000-02-07)", 
+#                         "Request(2, confirmed, 2000-01-01, 1, 1, 2000-02-05, 2000-02-07)", 
+#                         "Request(3, unseen3, 2000-01-01, 3, 3, 2000-02-01, 2000-03-01)"]
+    
 
-# def test_check_password(db_connection):
-#     db_connection.seed("seeds/makers_bnb.sql")
-#     repository = UserRepository(db_connection)
-#     assert repository.check_password("test@email.com", "test_password") == True
 
-# def test_check_incorrect_password(db_connection):
-#     db_connection.seed("seeds/makers_bnb.sql")
-#     repository = UserRepository(db_connection)
-#     assert repository.check_password("test2@email.com", "wrong_password") == False
 
-# def test_find_user(db_connection):
+# def test_find_request(db_connection):
 #     db_connection.seed("seeds/makers_bnb.sql")
-#     repository = UserRepository(db_connection)
-#     user = repository.find_user("test@email.com")
-#     assert user == "User(1, test_username, test@email.com, test_password)"
+#     repository = RequestRepository(db_connection)
+#     request = repository.find_request(1)
+#     assert request == "Request(1, unseen, 2000-01-01, 1, 1, 2000-02-05, 2000-02-07)"
+#     # check this is the same as JOINS on db
 
-# def test_find_user_invalid(db_connection):
+# def test_update_request_dates(db_connection):
 #     db_connection.seed("seeds/makers_bnb.sql")
-#     repository = UserRepository(db_connection)
-#     user = repository.find_user("invalid@email.com")
-#     assert user == None
+#     repository = RequestRepository(db_connection)
+#     request = repository.update_request_dates(2000-02-01, 2000-02-15, 1)
+#     assert request == "Request(1, Unseen, 2000-01-01, 1, 1, 2000-02-01, 2000-02-15)"
+    
+# def test_delete_request(db_connection):
+#     db_connection.seed("seeds/makers_bnb.sql")
+#     repository = RequestRepository(db_connection)
+#     repository.delete_request(3)
+#     request = repository.get_all_requests()
+#     assert request == 
 
-# def test_get_username(db_connection):
-#     db_connection.seed("seeds/makers_bnb.sql")
-#     repository = UserRepository(db_connection)
-#     username = repository.get_username(1)
-#     assert username == "test_username"
 
-# def test_get_username_invalid(db_connection):
-#     db_connection.seed("seeds/makers_bnb.sql")
-#     repository = UserRepository(db_connection)
-#     username = repository.get_username(len(db_connection.execute("SELECT * FROM users;")) + 1)
-#     assert username == None
+
+
